873f7bf306b586b920bd7a03189d9b1941a28a01
sohelranagtc <sohelranagtc@gmail.com>
2023-03-31 00:24:05 +0600

    Fakedata make and tailwind install

---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div>
+            <h1>Knowledge House</h1>
+            <img src="" alt="" />
+        </div>
+    );
+};
+
+export default Nav;
